8ee354890e7a603db2d7716dff4cf0cd5effe8db
fahmi <fahmihanif234@gmail.com>
2024-06-09 22:18:57 +0700

    upload

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -40,18 +40,15 @@ const About = () => {
     <>
       <motion.div variants={textVariant()}>
         <p className={styles.sectionSubText}>Introduction</p>
-        <h2 className={styles.sectionHeadText}>About PPLG</h2>
+        <h2 className={styles.sectionHeadText}>About Fahmi</h2>
       </motion.div>
 
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-center text-secondary text-[17px] leading-[30px]'
       >
-        I'm a skilled software developer with experience in TypeScript and
-        JavaScript, and expertise in frameworks like React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
-        create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
+        Saya adalah seorang siswa di SMK Citra Negara yang sedang mempelajari pemograman web, aplikasi serta games. 
+        Saya mempunyai impian sebagai web developer, game developer agar bisa membanggakan kedua orang tua saya.
       </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10'>
